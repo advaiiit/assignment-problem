d847237b380513a98d7b1b1b31c6800a25cbb28a
Advait Kamathe <akamathe@relatient.com>
2022-12-05 14:56:20 +0530

    Small modifications

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -51,14 +51,14 @@ public class Main {
             System.out.println();
         }
 
-        System.out.println("\n\n");
+        System.out.println("\n");
 
         HungarianAlgorithm hungarianAlgorithm = new HungarianAlgorithm(dataMatrix);
         int[][] optimalAssignment = hungarianAlgorithm.findOptimalAssignment();
 
         if (optimalAssignment.length > 0) {
             for (int i = 0; i < optimalAssignment.length; i++) {
-                System.out.print("Col: " + optimalAssignment[i][0] + "-> Row: " + optimalAssignment[i][1] +
+                System.out.print("Row: " + optimalAssignment[i][1] + "-> Col: " + optimalAssignment[i][0] +
                         " (" + dataMatrix[optimalAssignment[i][0]][optimalAssignment[i][1]] + ")");
                 System.out.println();
             }
